a99fa4c51622aac03ae2e0682292baac084281bd
Andy Wang <silverkni@gmail.com>
2017-10-17 23:28:00 -0700

    following new tutorial

--- test_flask_app/api.py
+++ test_flask_app/api.py
@@ -1,12 +1,12 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
 todos = {}
 
-class TodoSimple(Resource):
+class JobListAPI(Resource):
     def get(self, todo_id):
         return {todo_id: todos[todo_id]}
 
@@ -14,7 +14,10 @@ class TodoSimple(Resource):
         todos[todo_id] = request.form['data']
         return {todo_id: todos[todo_id]}
 
-api.add_resource(TodoSimple, '/<string:todo_id>')
+class JobAPI(Resource):
+    pass 
+
+api.add_resource(Tasks, '/<string:todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
