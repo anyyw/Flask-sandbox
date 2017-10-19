204592b720ccb734c7180317ddfb09c57a65f553
Andy Wang <silverkni@gmail.com>
2017-10-18 21:57:02 -0700

    further into the tutorial

--- test_flask_app/api.py
+++ test_flask_app/api.py
@@ -4,21 +4,22 @@ from flask_restful import Resource, Api, reqparse
 app = Flask(__name__)
 api = Api(app)
 
-todos = {}
+jobs = {}
 
 class JobAPI(Resource):
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+    def get(self, job_id):
+        return {job_id: jobs[job_id]}
 
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
+    def put(self, job_id):
+        jobs[job_id] = request.form['data']
+        return {job_id: jobs[job_id]}
 
 class JobListAPI(Resource):
     def get(self):
-        return todos 
+        return jobs 
 
-api.add_resource(Tasks, '/<string:todo_id>')
+api.add_resource(JobListAPI, '/job/api/v1.0/jobs', endpoint='jobs')
+api.add_resource(JobAPI, '/job/api/v1.0/jobs/<int:job_id>', endpoint='job')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
