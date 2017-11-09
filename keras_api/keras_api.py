be38de051a3f4cf8481c086a23fa70d8071e8c92
Andy Wang <silverkni@gmail.com>
2017-11-08 22:20:55 -0800

    Initial commit on Keras api with examples that will be calling it

---
+++ keras_api/keras_api.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, url_for, abort
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+jobs = {}
+            
+class JobAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type = str, location = 'json')
+        self.reqparse.add_argument('description', type = str, loctiton = 'json')
+        self.reqparse.add_argument('done', type = bool, location = 'json')
+        super(JobAPI, self).__init__()
+
+    def get(self, job_id):
+        job = [job for job in jobs if job['job_id'] == job_id]
+        if len(job) == 0:
+        	abort(404, message="Job {} does not exist".format(job_id))
+        return { 'job': id_to_uri(job) }
+
+    def put(self, job_id):
+        validate_jobid_exists(job_id)
+        args = self.reqparse.parse_args()
+        job = jobs[job_id]
+        for k, v in args.iteritems():
+            if v != None:
+                job[k] = v
+        return {job_id: jobs[job_id]}
+
+    def delete(self, job_id):
+    	validate_jobid_exists(job_id)
+
+
+class JobListAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type = str, required = True,
+            help = 'No Job title provided', location = 'json')
+        self.reqparse.add_argument('description', type = str, default = "", location = 'json')
+        super(JobListAPI, self).__init__()
+
+    def get(self):
+        return jobs 
+
+api.add_resource(JobListAPI, '/job/api/v1.0/jobs', endpoint='jobs')
+api.add_resource(JobAPI, '/job/api/v1.0/jobs/<int:job_id>', endpoint='job')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
